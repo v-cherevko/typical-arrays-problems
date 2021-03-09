5931a2f83266542006f500647dfbfe10a89d2822
ValeronChareuka <vchareuka8@gmail.com>
2021-03-09 15:40:18 +0300

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (!array) {
+        return 0;
+    }
+    if (array.length > 0) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array) {
+        return 0;
+    }
+    if (array.length > 0) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array) {
+        return 0;
+    }
+    if (array.length > 0) {
+        let sum = array.reduce((a, b) => a + b, 0);
+        return sum / array.length;
+    } else {
+        return 0;
+    }
+};
